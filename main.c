c4e1660eecf6fa4cecf7c4d2c1549ea2cf169556
velvelhype <velvelhype@gmail.com>
2021-10-07 14:27:32 +0900

    find square

--- main.c
+++ main.c
@@ -55,19 +55,11 @@ int		for_stdio_map()
 	return (SUCCESS);
 }
 
-int		for_arg_map(int argc, char *argv[], int i)
+int		find_square(char *content)
 {
-	int		ifd;
-	char	*content;
 	char	**map;
 	t_map_info	*info;
 
-	if ((ifd = open(argv[i], O_RDONLY)) == -1)
-		return (FAIL);
-	content = ft_read(ifd);
-	if (is_end_newline(content) == FAIL)
-		return (FAIL);
-	close(ifd);
 	map = ft_split(content, "\n");
 	free(content);
 	if (check_map_info(map) == FAIL)
@@ -77,8 +69,33 @@ int		for_arg_map(int argc, char *argv[], int i)
 	if (is_map_info_correct(map, info) == FAIL)
 		return (FAIL);
 	make_square(map, info);
-	if (!(i + 1 == argc))
-		ft_putstr("\n");
+	ft_free(&map);
+	free(info);
+	return (SUCCESS);
+}
+
+int		for_arg_map(char *argv[], int i)
+{
+	int		ifd;
+	char	*content;
+	char	**map;
+	t_map_info	*info;
+
+	if ((ifd = open(argv[i], O_RDONLY)) == -1)
+		return (FAIL);
+	content = ft_read(ifd);
+	close(ifd);
+	if (is_end_newline(content) == FAIL)
+		return (FAIL);
+	map = ft_split(content, "\n");
+	free(content);
+	if (check_map_info(map) == FAIL)
+		return (FAIL);
+	if (!(info = parse_map_info(map)))
+		return (FAIL);
+	if (is_map_info_correct(map, info) == FAIL)
+		return (FAIL);
+	make_square(map, info);
 	ft_free(&map);
 	free(info);
 	return (SUCCESS);
@@ -87,17 +104,31 @@ int		for_arg_map(int argc, char *argv[], int i)
 int		main(int argc, char *argv[])
 {
 	int i = 0;
+	char *content;
+
 	if (argc < 2)
 	{
-		if 	(for_stdio_map() == FAIL)
-		ft_putstr("map error\n");
+		content = ft_read(0);
+		if (is_end_newline(content) == SUCCESS)
+		{
+			if (find_square(content) == FAIL)
+				ft_putstr("map error\n");
+		}
+		else
+		{
+			ft_putstr("map error\n");
+		}
 	}
 	else
 	{
 		while (++i < argc)
 		{
-			if (for_arg_map(argc, argv, i) == FAIL)
+			//コンテンツ読み込み
+			//チェック
+			if (for_arg_map(argv, i) == FAIL)
 			ft_putstr("map error\n");
+			if (!(i + 1 == argc))
+				ft_putchar('\n');
 		} 
 	}
 	return (0);
